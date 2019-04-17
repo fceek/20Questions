b55ca3fbe87d7844fa5db079d26b6584f7226f9d
fceek <fceek96@gmail.com>
2019-04-17 02:01:27 +0100

    HTML/CSS finished

---
+++ scripts/narrow.js
@@ -0,0 +1,5 @@
+function introshow() {
+  let intro = document.querySelector("#intro-narrow");
+  if (intro.style.visibility != "visible") intro.style.visibility = "visible";
+    else intro.style.visibility = "hidden";
+}
